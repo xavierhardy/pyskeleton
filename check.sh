1acf3ebd2606806666518ff36c2bb1ee96b48360
Xavier Hardy <xavierhardy@users.noreply.github.com>
2020-03-17 17:32:45 +0000

    Remove tests.sh, enter the virtualenv in check.sh

--- check.sh
+++ check.sh
@@ -5,6 +5,14 @@ SOURCE_DIR="pyskeleton"
 TEST_DIR="tests"
 MIN_COVERAGE=80
 
+if [ ! "$(command -v black)" ] || [ ! "$(command -v flake8)" ] || [ ! "$(command -v shellcheck)" ] || [ ! "$(command -v yamllint)" ] || [ ! "$(command -v coverage)" ]; then
+  ./install.sh
+fi
+
+echo "Entering Python-3 virtual environment..."
+# shellcheck disable=SC1091
+. .venv/bin/activate
+
 echo "Checking code style with black"
 black --check "$SOURCE_DIR" "$TEST_DIR"
 echo
